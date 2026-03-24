19775ad5b61bf51ce143211d570b0cc43df543f9
Prashant Pal <prashant@Prashants-MacBook-Air.local>
2026-03-24 23:47:24 +0530

    new changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Update");
+  res.send("CI/CD Updated again");
 });
 
 app.listen(5000, () => console.log("Server running"));
