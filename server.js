1d3cee165020a57cf142bca77ca82ed90d2e642d
Prashant Pal <prashant@Prashants-MacBook-Air.local>
2026-03-24 23:42:53 +0530

    update app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working 🚀");
+  res.send("CI/CD Update");
 });
 
 app.listen(5000, () => console.log("Server running"));
